5a02443bcaf205bff3510f284511f263e5af337a
brayan-gh <20221106@uthh.edu.mx>
2025-11-04 23:34:12 -0600

    Actualiza expedienteApi.js

--- src/api/expedienteApi.js
+++ src/api/expedienteApi.js
@@ -3,8 +3,11 @@ import { EXPO_PUBLIC_API_URL } from "@env";
 export const getExpedienteById = async (id) => {
   try {
     const response = await fetch(`${EXPO_PUBLIC_API_URL}/getExpedienteById/${id}`);
+    const text = await response.text();
+
     if (!response.ok) throw new Error("Error al obtener el expediente");
-    return await response.json();
+
+    return JSON.parse(text);
   } catch (error) {
     console.error("❌ Error en getExpedienteById:", error);
     throw error;
