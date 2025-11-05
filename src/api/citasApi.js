6db153c65847adb1391fa7309b429ab4f7e29248
JoseFlores666 <20221043@uthh.edu.mx>
2025-11-04 23:08:36 -0600

    Actualizo carpeta api userApi y noticiasApi consultas

--- src/api/citasApi.js
+++ src/api/citasApi.js
@@ -1,12 +1,12 @@
 import { EXPO_PUBLIC_API_URL } from "@env";
 
-export const getCitasByDoctorId = async (codpaci) => {
+export const getCitasByDoctorId = async (id) => {
   try {
-    const response = await fetch(`${EXPO_PUBLIC_API_URL}/getCitasByPacienteId/${codpaci}`);
+    const response = await fetch(`${EXPO_PUBLIC_API_URL}/getCitasByPacienteId/${id}`);
     if (!response.ok) throw new Error("Error al obtener las citas");
     return await response.json();
   } catch (error) {
-    console.error("❌ Error en getCitasByDoctorId:", error);
+    console.error("Error en getCitasByDoctorId:", error);
     throw error;
   }
 };
